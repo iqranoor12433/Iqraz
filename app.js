b0392815bd59e8d57663e6d19fe8742c4cd0b0a8
iqranoor12433 <maniakakar@gmail.com>
2024-08-17 13:39:06 -0700

    my 1st repo

---
+++ app.js
@@ -0,0 +1,18 @@
+document.querySelector('.btn1').addEventListener('click',merafunction);
+function merafunction(){
+
+    let myCartItem=document.createElement('div');
+    let myimg=document.createElement('img');
+    myimg.className='mypic';
+    myimg.src='babbbb.png';
+    myCartItem.append(myimg);
+
+    myCartItem.className='cartDiv';
+
+
+    let cartbox=document.querySelector('.cart');
+    cartbox.append(myCartItem);
+
+    // document.querySelector('.cart').appendChild(myCartItem);
+}
+    
