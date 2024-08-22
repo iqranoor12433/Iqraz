6a26dd0cec1d8a9e9ea786413c8d5cd3309c5435
iqranoor12433 <maniakakar@gmail.com>
2024-08-22 08:56:14 -0700

    hamberger

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
-document.querySelector('.btn1').addEventListener('click',merafunction);
-function merafunction(){
+// document.querySelector('.btn1').addEventListener('click',merafunction);
+// function merafunction(){
 
-    let myCartItem=document.createElement('div');
-    let myimg=document.createElement('img');
-    myimg.className='mypic';
-    myimg.src='babbbb.png';
-    myCartItem.append(myimg);
+//     let myCartItem=document.createElement('div');
+//     let myimg=document.createElement('img');
+//     myimg.className='mypic';
+//     myimg.src='babbbb.png';
+//     myCartItem.append(myimg);
 
-    myCartItem.className='cartDiv';
+//     myCartItem.className='cartDiv';
 
 
-    let cartbox=document.querySelector('.cart');
-    cartbox.append(myCartItem);
+//     let cartbox=document.querySelector('.cart');
+//     cartbox.append(myCartItem);
 
-    // document.querySelector('.cart').appendChild(myCartItem);
-}
+//     // document.querySelector('.cart').appendChild(myCartItem);
+// }
     
+const bar = document.getElementById('bar');
+const nav = document.getElementById('pages');
+if (bar){
+    bar.addEventListener('click', ()=>{
+        nav.classList.add('active');
+    })
+}
